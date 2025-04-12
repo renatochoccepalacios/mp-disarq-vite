1a2c71ce929fcbb55f6f8fa766768ae42195e6d9
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-04-11 21:09:07 -0300

    diseno de las cards de servicio

--- src/components/ServicioSection.jsx
+++ src/components/ServicioSection.jsx
@@ -5,15 +5,19 @@ export const ServicioSection = () => {
     return (
         <section className=' relative'>
 
-            <div className=' py-6 z-10 px-[1.1rem] max-w-[800px] m-auto flex'>
-                <article  className='bg-white'>
+            <div className=' py-6 z-10 px-[1.1rem] max-w-[1260px] m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[3rem]'>
+                <article className='bg-white max-w-[240px] shadow-[0px_11px_10px_rgba(0,0,0,0.2)] rounded-[1rem] flex flex-col  w-full h-[400px]'>
                     <img src={prueba1} alt="" />
-                    <div className='min-h[400px] py-[2rem] px-[1.1rem]'>
-                        <h4>Dizse</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, aut, quo assumenda tenetur obcaecati facilis ea saepe, modi non impedit eos quod ad. Mollitia inventore laborum, nihil quidem tenetur neque!</p>
+                    <div className=' py-[2rem] px-[1.1rem]  flex gap-[2rem] flex-col justify-between'>
+                        <div>
+                            <h4>Diseño arquitectónico</h4>
+                            <p>Nos especializamos en la personalización de proyectos, diseñando cada detalle.
+
+</p>
+                        </div>
+                        <a href="" className='p-[0.55rem] w-[100px] text-center bg-black text-white'>Ver Mas</a>
                     </div>
                 </article>
-                
 
             </div>
             <div className='h-[200px] z-[-5]  bg-secundario absolute top-0 w-full'></div>
