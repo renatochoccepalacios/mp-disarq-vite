786582fee324300f04ce212c350d2ac3cdcf2999
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-04-15 19:39:05 -0300

    after en el banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,13 +4,13 @@ const Banner = () => {
     return (
         <section className='grid h-screen w-full text-white'>
             <img src="https://plus.unsplash.com/premium_photo-1679582752958-620b02eab7cc?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="absolute top-0 left-0 w-full h-full object-cover " />
-            <div className='grid relative z-20  w-full bg-gray-800/50 backdrop-blur-md p-4 '>
+            <div className='grid relative z-20  w-full bg-gray-20/50 backdrop-blur-sm p-4 banner'>
                 <div className='h-screen  absolute flex  items-center w-full  text-center justify-center md:justify-start'>
-                    <h1 className='text-4xl flex flex-col w-full items-center justify-center  text-[4rem] max-w-[550px] tracking-[0.25rem] uppercase font-thin text-center '>Mp disarq
+                    <h1 className='text-4xl z-10 flex flex-col w-full items-center justify-center  text-[4rem] max-w-[700px] tracking-[0.25rem] uppercase font-thin text-center '>Mp disarq
                         <span className='tracking-[3px] text-[14px]'>Diseño y Construcciones</span>
                     </h1>
                 </div>
-                <span className='absolute bottom-[90px] w-full text-center px-[2rem] text-[.80rem]'>Diseño y Construcciones Un buen diseño consiste en convertir sueños en realidad.</span>
+                <span className='absolute bottom-[90px] z-10 w-full text-center px-[2rem] text-[.80rem]'>Diseño y Construcciones Un buen diseño consiste en convertir sueños en realidad.</span>
             </div>
 
         </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,11 @@ import { HiX } from "react-icons/hi";
 export const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
+
     const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
-        <header className='grid  z-50 fixed w-full bg-secundario gap-[2rem] text-black'>
+        <header className='grid  z-50 fixed w-full bg-acento  gap-[2rem] '>
             {/* Overlay */}
             {isOpen && (
                 <div
@@ -15,14 +16,14 @@ export const NavBar = () => {
                     onClick={toggleMenu} // Opcional: cerrar al hacer click en el fondo
                 />
             )}
-            <nav className='grid z-50 max-w-[1260px] bg-secundario  m-auto md:flex p-4 relative grid-cols-2 gap-2.5 md:grid-cols-2 justify-between w-full items-center'>
-                <h1 className='text-2xl font-bold'>My Website</h1>
+            <nav className='grid z-50 max-w-[1260px] bg-acento text-principal  m-auto md:flex p-4 relative grid-cols-2 gap-2.5 md:grid-cols-2 justify-between w-full items-center' >
+                <h1 className='text-2xl font-bold'>Mp Disarq</h1>
 
                 <button onClick={toggleMenu} className='z-10 justify-self-end items-center md:hidden cursor-pointer'>
                     {isOpen ? <HiX className='text-2xl' /> : <GiHamburgerMenu className='text-2xl' />}
                 </button>
 
-                <ul className={`p-4 order-2 w-[200px] absolute  top-full left-0 bg-suave  backdrop-blur-md flex gap-3 flex-col transition-all duration-300 ease-in-out ${isOpen ? 'menu-open ' : 'menu-closed'} h-screen md:static md:flex-row md:justify-center md:gap-[4rem] md:w-auto md:p-0 md:h-auto md:backdrop-blur-0 md:bg-transparent`}>
+                <ul className={`p-4 order-2 w-[200px] absolute  top-full left-0 bg-acento  backdrop-blur-md flex gap-3 flex-col transition-all duration-300 ease-in-out ${isOpen ? 'menu-open ' : 'menu-closed'} h-screen md:static md:flex-row md:justify-center md:gap-[4rem] md:w-auto md:p-0 md:h-auto md:backdrop-blur-0 md:bg-transparent`}>
                     <li><a href="#home" className='uppercase'>Home</a></li>
                     <li><a href="#about" className='uppercase'>About</a></li>
                     <li><a href="#services" className='uppercase'>Services</a></li>
